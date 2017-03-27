a720cf7d9d7c3b5af89ff3fcf040adafcd4d8f2f
Spencer Julian <helloThere@spencerjulian.com>
2017-03-26 22:25:08 -0700

    Properly document app.py

--- aniping/app.py
+++ aniping/app.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Aniping.
+
+This is the main entrypoint for the application. For full documentation on what
+aniping is, please check README.rst.
+
+All processing in the aniping package should start through the front_end submodule,
+which will itself call the requisite modules. The exception is config loading,
+which should be done when the server loads for the first time.
+
+When the wsgi app is built, an apscheduler instance is attached to it to schedule
+a weekly job to scrape shows from our sources.
+"""
 from flask import render_template, session, request, make_response, escape, jsonify, abort, redirect
 from urllib.parse import unquote_plus
 from aniping import front_end, config
@@ -6,6 +18,8 @@ from flask_apscheduler import APScheduler
 import os, time, sys, datetime, atexit, logging
 
 app = application = config.Flask(__name__, template_folder='views', static_folder='static')
+"""Flask: WSGI Application entry point"""
+
 app.config.from_yaml(os.environ.get('ANIPING_CONFIG', os.path.join(app.root_path, 'config/config.yml')))
 
 if app.config["DEBUG"]:
@@ -15,6 +29,7 @@ else:
     logging.basicConfig(level=logging.INFO)
     
 log = logging.getLogger(__name__)
+"""logger: Logging endpoint"""
         
 app.config["JOBS"] = [ {
     'id': 'scrape_job',
@@ -28,12 +43,27 @@ app.config["JOBS"] = [ {
 app.config["SCHEDULER_API_ENABLED"] = True
 
 scheduler = APScheduler()
+"""APScheduler(): APScheduler instance to schedule occasional tasks"""
 scheduler.init_app(app)
 scheduler.start()
 
 @app.route('/search')
 @app.route('/')
 def index():
+    """Primary index function.
+
+    This function handles searching and the main page. If ``q`` is passed in a query
+    string, e.g. ``http://localhost?q=gabriel+dropout``, then a search will be performed.
+
+    If request path is ``search``, e.g. ``http://localhost/search``, then the navigation
+    menu will not be rendered.
+
+    Should there be no shows returned from the backend, ``front_end.do_first_time_setup``
+    will be called to scrape shows from the source.
+
+    Returns:
+        A rendered template, either ``first_time.html`` for the first run or ``default.html`` otherwise.
+    """
     log.debug("Entering index, attempting to get shows.")
     watching, airing, specials, movies = front_end.get_shows_for_display(app.config, request.args.get('q',None))
     standalone = True if request.path.strip('/') == 'search' else False
@@ -46,6 +76,15 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """Login POST handler.
+
+    Only runs when ``/login`` is hit with a POST method. There is no GET method
+    equivilent, as it is handled by the navigation template. Sets the status
+    code to ``401`` on login failure.
+
+    Returns:
+        JSON formatted output describing success or failure.
+    """
     log.debug("Entering login, attempting to authenticate user.")
     username = request.form['signin_username']
     password = request.form['signin_password']
@@ -62,6 +101,13 @@ def login():
 
 @app.route('/logout')
 def logout():
+    """Logout handler.
+
+    Ends the client session and deletes the session ID from the database.
+
+    Returns:
+        JSON formatted output describing success.
+    """
     log.debug("Entering logout, attempting to end session.")
     front_end.delete_login_id(escape(session['logged_in']))
     session.pop('logged_in', None)
@@ -70,6 +116,15 @@ def logout():
 
 @app.route('/star')
 def star():
+    """Starring/Highlighting handler.
+
+    Attempts to toggle a star/highlight on a particular show. The show ID must
+    be passed in the ``id`` query string. If the user is unauthenticated, the
+    function is aborted with a ``404`` message to hide the page.
+
+    Returns:
+        JSON formatted output describing success and the ID of the show starred.
+    """
     log.debug("Entering star, trying to toggle star.")
     if front_end.check_login_id(escape(session['logged_in']), app.config):
         log.debug("Sending show ID {0} to function".format(request.args['id']))
@@ -81,6 +136,15 @@ def star():
     
 @app.route('/rm')
 def drop_show():
+    """Show removal handler.
+
+    Attempts to remove a show from the backend system. The show ID must
+    be passed in the ``id`` query string. If the user if unauthenticated, the
+    function is aborted with a ``404`` message to hide the page.
+
+    Returns:
+        An HTTP redirect to the home page, to refresh.
+    """
     log.debug("Entering drop_show, trying to remove show from list.")
     if front_end.check_login_id(escape(session['logged_in']), app.config):
        log.debug("Sending show ID {0} to function".format(request.args['id']))
@@ -93,6 +157,28 @@ def drop_show():
 @app.route('/edit', methods=['GET', 'POST'])
 @app.route('/add', methods=['GET', 'POST'])
 def update_show():
+    """Show add and edit handler.
+
+    Either displays a template allowing the user to edit or add a show, or attempts
+    to edit or add the show, depending on if the method is GET or POST. The function
+    is aborted with a ``404`` message to hide the page if the user is not authenticated.
+
+    GET method:
+        Requires ``id`` to be passed as the ID of the show in a query string. If
+        the show can't be found, abort with a ``404`` message. Otherwise, lookup
+        the show in the db, as well as sub groups subtitling the show and the selected
+        sub group if there is one, and some fanart to render on the background.
+    
+    POST method:
+        Requires ``id``, ``beid``, and ``subgroup`` to be passed as form parameters.
+        ``id`` is the DB id of the show, ``beid`` is the backend ID of the show, and
+        ``subgroup`` is the subgroup the user has selected. This will attempt to add
+        the show to the backend.
+
+    Returns:
+        A rendered template with a form on the GET method.
+        A redirect to the home as a refresh on the POST method.
+    """
     log.debug("Entering update_show, trying to {0} show".format(request.path.strip('/')))
     logged_in = front_end.check_login_id(escape(session['logged_in']), app.config)
     if logged_in and request.method == 'POST':
